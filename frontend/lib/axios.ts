c22a2092fb19eed0fe293f919dd7d42b6dd1f75b
Tez-Saikia <tezsaikia14@gmail.com>
2026-07-13 15:43:25 +0530

    axios

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-console.log(
-  "NEXT_PUBLIC_API_URL:",
-  process.env.NEXT_PUBLIC_API_URL
-);
+console.log("NEXT_PUBLIC_API_URL =", process.env.NEXT_PUBLIC_API_URL);
 
 export const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
