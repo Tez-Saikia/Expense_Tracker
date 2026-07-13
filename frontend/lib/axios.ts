5cf819490040a7952fe8931e67c40f5532b74024
Tez-Saikia <tezsaikia14@gmail.com>
2026-07-13 15:20:53 +0530

    fixed axios url

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -1,11 +1,6 @@
 import axios from "axios";
 
-const baseURL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:4000/api/v1/"
-    : "/user";
-
 export const axiosInstance = axios.create({
-  baseURL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
 });
